a0b54f989a089e19f6f567333958a1869641e41e
Lui Wolff <luimlask@gmail.com>
2021-04-07 17:22:36 -0300

    projectile

--- Scripts/shell_test.gd
+++ Scripts/shell_test.gd
@@ -7,7 +7,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	life -= delta
+	life -= (delta/2)
 	transform.origin += transform.basis.z * delta * 100 #Multiplying by delta to prevent framerate-dependent shell speed
 	rotate(transform.basis.x, delta/15)
 	if life < 0.0:
