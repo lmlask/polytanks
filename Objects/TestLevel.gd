2787840cb4271bfdc763a028d55c222b4808c559
Chris <chrisneilbrown@gmail.com>
2021-04-14 19:44:17 +1000

    fixed normals

--- Objects/TestLevel.gd
+++ Objects/TestLevel.gd
@@ -10,7 +10,7 @@ extends Spatial
 func _ready():
 	var mesh = $MeshInstance.mesh.duplicate()
 	var mat = $MeshInstance.get_surface_material(0)
-	print(mat)
+#	print(mat)
 	var noise = R.NoiseTexture.texture.noise
 
 	# Configure
@@ -22,13 +22,46 @@ func _ready():
 	# Sample
 		
 	var mdt = MeshDataTool.new()
+#	var mat2 = mdt.get_material()
 	mdt.create_from_surface(mesh, 0)
+	print(mdt.get_vertex(6538))
+	print(mdt.get_vertex_normal(6538))
 	for i in range(mdt.get_vertex_count()):
 		var vertex = mdt.get_vertex(i)
 		vertex.y = noise.get_noise_2d(vertex.x+translation.x, vertex.z+translation.z)*25
 		mdt.set_vertex(i, vertex)
+#		mdt.set_vertex_normal(i,vertex)
+
+
+	for i in range(mdt.get_face_count()):
+		# Get the index in the vertex array.
+		var a = mdt.get_face_vertex(i, 0)
+		var b = mdt.get_face_vertex(i, 1)
+		var c = mdt.get_face_vertex(i, 2)
+		# Get vertex position using vertex index.
+		var ap = mdt.get_vertex(a)
+		var bp = mdt.get_vertex(b)
+		var cp = mdt.get_vertex(c)
+		# Calculate face normal.
+		var n = (bp - cp).cross(ap - bp).normalized()
+		# Add face normal to current vertex normal.
+		# This will not result in perfect normals, but it will be close.
+		mdt.set_vertex_normal(a, n + mdt.get_vertex_normal(a))
+		mdt.set_vertex_normal(b, n + mdt.get_vertex_normal(b))
+		mdt.set_vertex_normal(c, n + mdt.get_vertex_normal(c))
+
+	# Run through vertices one last time to normalize normals and
+	# set color to normal.
+	for i in range(mdt.get_vertex_count()):
+		var v = mdt.get_vertex_normal(i).normalized()
+		mdt.set_vertex_normal(i, v)
+#		mdt.set_vertex_color(i, Color(v.x, v.y, v.z))
+#		mdt.set_vertex_color(i, Color(1,1,1))
+
+
 	mesh.surface_remove(0)
 	mdt.commit_to_surface(mesh)
+#	mdt.set_material(mat)
 	$MeshInstance/StaticBody/CollisionShape.shape = mesh.create_trimesh_shape()
 	$MeshInstance.mesh = mesh
 	$MeshInstance.set_surface_material(0, mat)
