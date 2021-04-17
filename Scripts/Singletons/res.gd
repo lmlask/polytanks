6382958b32dbbc80791ff91c5dd855e47f86cfff
Chris <chrisneilbrown@gmail.com>
2021-04-18 02:16:13 +1000

    added house models

--- Scripts/Singletons/res.gd
+++ Scripts/Singletons/res.gd
@@ -18,5 +18,5 @@ onready var MHouse = preload("res://models/house.tscn") #Model House
 onready var BerHouseS1 = preload("res://models/Berberhouse_small01.tscn")
 onready var BerHouseT2 = preload("res://models/BerberHouse02_v2.tscn")
 onready var BerHouseT3 = preload("res://models/BerberHouse03_v2.tscn")
-onready var BerHouseT4 = preload("res://models/BerberHouse04_temp.tscn")
+onready var BerHouseT4 = preload("res://models/BerberHouse04_v2.tscn")
 onready var DebugUI = preload("res://Scenes/DebugUI.tscn")
