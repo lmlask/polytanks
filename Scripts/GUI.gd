311a958a7c64f28a89ca11acd43c2133fcba8022
Lui Wolff <luimlask@gmail.com>
2021-04-22 16:42:43 -0300

    int

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -1,20 +1,5 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func set_compass(b):
 	visible = GameState.WindView #Fix me only needs to be set once
 	var s = GameState.wind_vector.length()/100
