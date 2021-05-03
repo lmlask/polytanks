e1ccd932350472bfe0991d0411cd606f71c4c005
Chris <chrisneilbrown@gmail.com>
2021-05-03 10:48:09 +1000

    removed obsolete code in map manager

--- Scripts/MapManager.gd
+++ Scripts/MapManager.gd
@@ -14,27 +14,27 @@ var tile_offset = [Vector3(1,0,0),Vector3(0,0,1),Vector3(-1,0,0),Vector3(0,0,-1)
 #					Vector3(1,0,3),Vector3(0,0,3),Vector3(-1,0,3),Vector3(3,0,1),Vector3(3,0,0),Vector3(3,0,-1),
 #					Vector3(1,0,-3),Vector3(0,0,-3),Vector3(-1,0,-3),Vector3(-3,0,1),Vector3(-3,0,0),Vector3(-3,0,-1)]
 #var tile_offset = [Vector3(0,0,1)]
-var tran_offset = [Vector3(0,0,2)]
-var rough_offset = [Vector3(0,0,3)]
+#var tran_offset = [Vector3(0,0,2)]
+#var rough_offset = [Vector3(0,0,3)]
 
-var mesh10:ArrayMesh = ArrayMesh.new()
-var mesh25:ArrayMesh = ArrayMesh.new()
-var mesh100:ArrayMesh = ArrayMesh.new()
-var MapNode = null
-var thread_update = Thread.new()
-var map_thread = Thread.new()
-var mutex = Mutex.new()
+#var mesh10:ArrayMesh = ArrayMesh.new()
+#var mesh25:ArrayMesh = ArrayMesh.new()
+#var mesh100:ArrayMesh = ArrayMesh.new()
+#var MapNode = null #obsolete?
+#var thread_update = Thread.new()
+#var map_thread = Thread.new()
+#var mutex = Mutex.new()
 var fine_size = 16 #change array size
-var rough_size = 100
+#var rough_size = 100
 var map_size = 4
-var tilemesh = {} #Should be obsolete
+#var tilemesh = {} #Should be obsolete
 var terrainMeshs = {0:[],1:[],2:[],3:[],4:[]} #Center
 var terrainNodes = {}
 enum State {START, TANK, MAP, COMPLETE}
 var TerrainState = State.START
 
-var terrainTiles = {} #obsolete
-enum tileRes {ROUGH, FINE, A,B,C,D}#obsolete
+#var terrainTiles = {} #obsolete
+#enum tileRes {ROUGH, FINE, A,B,C,D}#obsolete
 var sites:Dictionary #0=Name
 var sitesID:int = 0
 var site_selected:int = -1
@@ -57,14 +57,6 @@ func _ready():
 	$DirectionalLight.rotation.x = -0.5
 	$DirectionalLight.light_energy = 1
 
-	
-#	maps[0] = preload("res://Objects/TestLevel.tscn")
-#	maps[1] = preload("res://Objects/CityLevel.tscn")
-#	maps[2] = preload("res://Objects/hills map.tscn")
-#	load_map(0,Vector3.ZERO)
-	
-	#Create a plane used for terrain
-#	map_thread.start(self,"create_mesh", fine_size)
 	var terrain_size = fine_size
 	for i in range(6): #change here
 		terrainMeshs[0].append(create_mesh(terrain_size))
@@ -73,19 +65,6 @@ func _ready():
 		terrainMeshs[3].append(rotate_mesh(terrainMeshs[2][i])) #Correct
 		terrainMeshs[4].append(rotate_mesh(terrainMeshs[3][i])) #Correct
 		terrain_size = terrain_size*2
-#	terrainMeshs.append(create_tran_mesh(fine_size, rough_size)) #Correct
-#	terrainMeshs.append(create_tran_mesh(fine_size, rough_size)) #Correct
-#	terrainMeshs.append(create_tran_mesh(fine_size, rough_size)) #Correct
-	
-#	terrainMeshs.append(create_mesh(rough_size))
-#	terrainMeshs.append(create_mesh(rough_size))
-#	terrainMeshs.append(create_mesh(rough_size))
-##	terrainMeshs.append(create_mesh(rough_size))
-
-#	terrainMeshs.append(create_tran_mesh(fine_size, rough_size)) #Correct
-#	terrainMeshs.append(rotate_mesh(terrainMeshs[2]))
-#	terrainMeshs.append(rotate_mesh(terrainMeshs[3]))
-#	terrainMeshs.append(rotate_mesh(terrainMeshs[4]))
 
 func load_files():
 	var file = File.new()
@@ -150,10 +129,6 @@ func create_tran_mesh(fine, rough):
 	arrays[ArrayMesh.ARRAY_INDEX] = Idx
 	# Create the Mesh.
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-#	mutex.lock()
-#	tilemesh[size] = mesh
-#	mutex.unlock()
-#	emit_signal("flat_terrain_completed")
 	return mesh
 
 func rotate_mesh(meshx):
@@ -177,16 +152,7 @@ func create_mesh(size)->ArrayMesh:
 		for x in range(0,1024+1,size):
 			vertices.push_back(Vector3(x, 0, z))
 			UVs.push_back(Vector2(x/1024.0, z/1024.0))
-#			vertices.push_back(Vector3(x+size, 0, z))
-#			UVs.push_back(Vector2((x+size)/1000.0, z/1000.0))
-#			vertices.push_back(Vector3(x, 0, z+size))
-#			UVs.push_back(Vector2(x/1000.0, (z+size)/1000.0))
-#			vertices.push_back(Vector3(x+size, 0, z))
-#			UVs.push_back(Vector2((x+size)/1000.0, z/1000.0))
-#			vertices.push_back(Vector3(x+size, 0, z+size))
-#			UVs.push_back(Vector2((x+size)/1000.0, (z+size)/1000.0))
-#			vertices.push_back(Vector3(x, 0, z+size))
-#			UVs.push_back(Vector2(x/1000.0, (z+size)/1000.0))
+
 	var row = 1024/size+1
 	for y in range(0,(1024/size)):
 		for x in range(0,(1024/size)):
@@ -198,10 +164,6 @@ func create_mesh(size)->ArrayMesh:
 			Idx.push_back(x+y*row+1)
 			Idx.push_back(x+(y+1)*row+1)
 			Idx.push_back(x+(y+1)*row)
-	# Initialize the ArrayMesh.
-#	print(vertices)
-#	print(UVs)
-#	print(Idx)
 
 	var mesh = ArrayMesh.new()
 	var arrays = []
@@ -218,13 +180,6 @@ func create_mesh(size)->ArrayMesh:
 	return mesh
 
 func clear_map():
-	if map_thread.is_active():
-		map_thread.wait_to_finish()
-	if thread_update.is_active():
-		thread_update.wait_to_finish()
-	if MapNode:
-		remove_child(MapNode)
-		MapNode.queue_free()
 	for i in R.VPlayers.get_children():
 		R.VPlayers.remove_child(i)
 		i.free()
@@ -238,14 +193,10 @@ func clear_map():
 		i.queue_free()
 	map = null
 	prev_tile = Vector3.INF
-	mutex.lock()
-	terrainTiles.clear()
 	maptiles.clear()
 	maptiles_size.clear()
 	terrainNodes.clear()
 	TerrainState = State.START
-	mutex.unlock()
-#	remove_items()
 	remove_locations()
 	for _wait in range(10): #Bad solution to possibly fix a weird bug
 		yield(get_tree(),"idle_frame")
@@ -273,47 +224,6 @@ func process_tile(pos, level):
 		terrainNodes[grid] = MapNode
 		MapNode.set_pos(grid)
 	MapNode.level = level
-	
-#	add_child(MapNode)
-#	MapNode.load_image(map)
-#	process_tile(Vector3(0,0,0),tileRes.FINE) 
-#	check_area(pos)
-#	update_tile([Vector3(0,0,0),fine_size])
-#	maptiles_size[Vector3(0,0,0)] = fine_size
-#	MapNode.update_tile(tilemesh[fine_size],maptiles[Vector3(0,0,0)])
-	
-	
-#	map.get_node("DirectionalLight").show()
-	
-	#Dont expand on this. the map itself should do this
-#	if map == 1 and false: #fix later
-#		for i in range(10):
-#			for j in range(10):
-#				var house = map.get_node("house").duplicate()
-#				house.translation.z += 15 * i
-#				house.translation.x += 15 * j
-#				map.add_child(house)
-
-func process_tile2(pos, level): #obsolete
-	return
-	var grid = R.pos2grid(pos)
-	var delete = null
-#	mutex.lock()
-	if terrainTiles.has(grid):
-		if terrainTiles[grid][0] == level:
-#			print("tile %s at level %s"  %[grid,level])
-			return
-		delete = terrainTiles[grid][1]
-	else:
-		terrainTiles[grid] = [null,null]
-#	terrainTiles[grid][1] = MapNode.add_tile(grid,terrainMeshsC[level])
-	terrainTiles[grid][0] = level
-	if delete:
-		delete.queue_free()
-#	maptiles[pos] = MapNode.add_tile(pos,terrainMeshs[0])
-#	maptiles_size[pos] = rough_size
-#	mutex.unlock()
-	emit_signal("terrain_completed", grid)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -340,79 +250,8 @@ func check_area(pos):
 	prev_tile = grid
 #	print("process tiles")
 	get_tree().call_group("terrain","update_tile",grid)
-	return
-	for i in tile_offset:
-		process_tile(pos+i*1024,0)
-#	var tran_side = 0
-	return
-	for i in tran_offset:
-		process_tile(pos+i*1024,0)
-##		print("offset",R.pos2grid(pos+i*1000))
-#		for i in j:
-#			match tran_side:
-#				0:
-#					process_tile(pos+i*1024,tileRes.A)
-#				1:
-#					process_tile(pos+i*1024,tileRes.D)
-#				2:
-#					process_tile(pos+i*1024,tileRes.C)
-#				3:
-#					process_tile(pos+i*1024,tileRes.B)
-#		tran_side += 1
-#	for i in rough_offset:
-#		process_tile(pos+i*1000,tileRes.ROUGH)
-	return
-	var size = fine_size
-	if not tilemesh.has(size):
-		return
-	pos.y = 0
-	var center = (pos/1024).snapped(Vector3(1,10,1))
-	if center == prev_tile:
-		return
-	prev_tile = center
-#	for j in tile_offset:
-#		MapNode.add_sites(center+j)
-	
-	if not MapNode.img_loaded:
-		return
-#	return
-	for i in tile_offset:
-		process_tile(pos+i*1024,tileRes.TRAN)
-		return
-		if not maptiles.has(center+i):
-			mutex.lock()
-			maptiles_size[center+i] = rough_size
-			maptiles[center+i] =  MapNode.add_tile(center+i,tilemesh[rough_size])
-			mutex.unlock()
-			prev_tile = Vector3.INF
-			break
-		if maptiles_size[center+i] == rough_size:
-			if not thread_update.is_active() and GameState.InGame:
-				thread_update.start(self, "update_tile", [center+i, size])
-			else:
-				prev_tile = Vector3.INF
-
-func update_tile(data):
-	var pos = data[0]
-	var size = data[1]
-#	print("update", pos, "-", size)
-	if not tilemesh.has(size):
-		print("tile mesh not ready")
-		prev_tile = Vector3.INF
-		thread_update.call_deferred("wait_to_finish")
-		return
-	mutex.lock()
-	maptiles_size[pos] = size
-	mutex.unlock()
-	MapNode.update_tile(tilemesh[size],maptiles[pos])
-	emit_signal("terrain_completed",pos)
-	thread_update.call_deferred("wait_to_finish")
 	
 func terrain_complete(grid):
-#	print("grid", grid)
-#	print("terrain complete", grid)
-#	var pos = R.ManVehicle.vehicle.global_transform.origin
-#	var grid = (pos/1000).snapped(Vector3(1,10,1))
 	if not TerrainState == State.COMPLETE:
 		if TerrainState == State.START and GameState.InGame:
 			TerrainState = State.TANK
@@ -442,30 +281,6 @@ func terrain_complete(grid):
 			if i.is_in_group("item"): #Fix this up, sort buildings/items or group them something better then "item"
 				grid = (i.global_transform.origin/1000).snapped(Vector3(1,10,1))
 				R.FloorFinder.find_floor2(i)
-#	for i in SitesNode.get_children():
-#		R.FloorFinder.find_floor2(i)
-
-#func add_tiles(pos,size = rough_size): #Not used?
-#	print("is this used?")
-#	for i in tile_offset:
-#		if not maptiles.has(pos+i):
-#			mutex.lock()
-#			maptiles[pos+i] =  MapNode.add_tile(pos+i,tilemesh[size])
-#			maptiles_size[pos+i] = size
-#			mutex.unlock()
-		
-func generate_map(mesh):
-#	for x in range(-5,5):
-#		for y in range(-5,5):
-	var pos = Vector3(0,0,0)
-	mutex.lock()
-	maptiles[pos] = MapNode.add_tile(pos,mesh)
-	maptiles_size[pos] = rough_size
-	mutex.unlock()
-
-#func _exit_tree():
-#	if thread_update.is_active():
-#		thread_update.wait_to_finish()
 
 func show_locations():
 	while LocsNode.get_child_count():
