edc9511ab939fdac87548528903aa8e4ff39c153
Chris <chrisneilbrown@gmail.com>
2021-05-25 22:47:39 +1000

    flat areas with control nodes

--- Scripts/Paint.gd
+++ Scripts/Paint.gd
@@ -56,9 +56,10 @@ func area(a):
 	line.append(a)
 #	print(line)
 	if line.size() == 2:
+		line.append(50)
 		R.ManMap.areas[R.ManMap.areas.size()] = line.duplicate()
 		line.clear()
-		add_road(R.ManMap.areas.size()-1)
+		process_road(R.ManMap.areas.size()-1,true)
 
 func handle_select(handle):
 	if selected_handle:
@@ -74,7 +75,17 @@ func _input(event):
 				get_tree().set_input_as_handled()
 
 func update_area(handle):
-	handle.owner.update_handle(handle)
+	var aid = handle.get_meta("AreaID")
+	var cid = handle.get_meta("ControlID")
+	print("update %s %s" % [aid, cid])
+	if cid < 2:
+		R.ManMap.areas[aid][cid] = handle.global_transform.origin
+	if cid == 2:
+		var G = Geometry
+		var p = G.get_closest_point_to_segment(handle.global_transform.origin,R.ManMap.areas[aid][0],R.ManMap.areas[aid][1])
+		R.ManMap.areas[aid][2] = handle.global_transform.origin.distance_to(p)
+	process_road(aid,false)
+#	handle.owner.update_handle(handle)
 	
 
 func _process(delta):
@@ -83,42 +94,52 @@ func _process(delta):
 		selected_handle.transform.origin -= selected_handle.transform.basis.x * R.Editor.move.x * delta * move_speed
 		selected_handle.transform.origin -= selected_handle.transform.basis.y * R.Editor.move.y * delta * move_speed
 
-func add_road(i):
-	var width = 50
+func process_road(i,new):
+	
 	var line = R.ManMap.areas[i]
-#	print("obsolete")
 #	line[0] = line[0]
 #	line[1] = line[1]
-	var CN1 = CN.duplicate()
-	var CN2 = CN.duplicate()
-	var CN3 = CN.duplicate()
-	AreaCN.add_child(CN1)
-	AreaCN.add_child(CN2)
-	AreaCN.add_child(CN3)
-	CN1.translation = line[0]
-	CN2.translation = line[1]
-	CN1.show()
-	CN2.show()
+	if new:
+		var CN1 = CN.duplicate()
+		var CN2 = CN.duplicate()
+		CN1.set_meta("AreaID", i)
+		CN1.set_meta("ControlID", 0)
+		CN2.set_meta("AreaID", i)
+		CN2.set_meta("ControlID", 1)
+		AreaCN.add_child(CN1)
+		AreaCN.add_child(CN2)
+		CN1.translation = line[0]
+		CN2.translation = line[1]
+		CN1.show()
+		CN2.show()
 	var G = Geometry
-	var node = R.Map.terrainNodes[Vector3(0,0,0)]
+#	var node = R.Map.terrainNodes[Vector3(0,0,0)]
 	var line_vec = (line[1]-line[0]).normalized()
-	var dist = line[0].distance_to(line[1])
+#	var dist = line[0].distance_to(line[1])
 	var tang = Transform.looking_at(line_vec,Vector3.UP).basis.x
-	var rs1 = line[0]+tang*width
-	var rs2 = line[0]-tang*width
-	var re1 = line[1]+tang*width
-	var re2 = line[1]-tang*width
+	var rs1 = line[0]+tang*line[2]
+	var rs2 = line[0]-tang*line[2]
+	var re1 = line[1]+tang*line[2]
+	var re2 = line[1]-tang*line[2]
 	var area:PoolVector3Array
 	area = [rs2,rs1,re1,re2]
-	CN3.translation = (line[0]+line[1])/2+tang*width
-	CN3.show()
+	
+	if new:
+		var CN3 = CN.duplicate()
+		CN3.set_meta("AreaID", i)
+		CN3.set_meta("ControlID", 2)
+		AreaCN.add_child(CN3)
+		CN3.translation = (line[0]+line[1])/2+tang*line[2]
+		CN3.show()
 
+	FlatG.clear()
 	FlatG.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
 	FlatG.add_vertex(rs1)
 	FlatG.add_vertex(rs2)
 	FlatG.add_vertex(re1)
 	FlatG.add_vertex(re2)
 	FlatG.end()
+	LineG.clear()
 	LineG.begin(Mesh.PRIMITIVE_LINES)
 	for p in area:
 		LineG.add_vertex(rs1)
@@ -156,8 +177,12 @@ func add_road(i):
 #	else:
 #		print("good")
 	var size = R.heightMap.get_size()
-	var c1 = R.heightMap.get_pixelv(start)
-	var c2 = R.heightMap.get_pixelv(end)
+	
+	var col = sqrt(line[0].y/200)/1.75
+	var c1 = Color(col,col,col,1)
+	col = sqrt(line[1].y/200)/1.75
+	var c2 = Color(col,col,col,1)
+	
 	var d2 = start.distance_to(end)
 	for x in range(size.x):
 		for y in range(size.y):
@@ -168,38 +193,38 @@ func add_road(i):
 				R.heightMap.set_pixelv(imgPoint,lerp(c1,c2,d1/d2))
 	
 	return
-	var mesh = node.get_node("Tile").mesh.duplicate()
-	var mdt = MeshDataTool.new()
-	mdt.create_from_surface(mesh, 0)
-	LineG.clear()
-	for i in range(mdt.get_edge_count()):
-		var e0 = mdt.get_edge_vertex(i, 0)
-		var e1 = mdt.get_edge_vertex(i, 1)
-		var v0 = mdt.get_vertex(e0)
-		var v1 = mdt.get_vertex(e1)
-		var point = {}
-		
-		for z in range(2):
-			for j in [[rs1,re1],[rs2,re2],[rs1,rs2],[re1,re2]]:
-				if G.segment_intersects_segment_2d(R.v3xz(v0),R.v3xz(v1),R.v3xz(j[0]),R.v3xz(j[1])):
-					var y = G. get_closest_points_between_segments(v0,v1,j[0],j[1])[1].y
-					v0 = Vector3(v0.x,y,v0.z)
-					v1 = Vector3(v1.x,y,v1.z)
-					mdt.set_vertex(e0,v0)
-					mdt.set_vertex(e1,v1)
-					
-					LineG.begin(Mesh.PRIMITIVE_LINES)
-					LineG.add_vertex(v0+node.translation)
-					LineG.add_vertex(v1+node.translation)
-					LineG.end()
-		for z in range(3):
-			if G.is_point_in_polygon(R.v3xz(v0),rect) or G.is_point_in_polygon(R.v3xz(v1),rect):
-				var y0 = G.get_closest_point_to_segment(v0,line[0],line[1]).y
-				var y1 = G.get_closest_point_to_segment(v1,line[0],line[1]).y
-				v0 = Vector3(v0.x,y0,v0.z)
-				v1 = Vector3(v1.x,y1,v1.z)
-				mdt.set_vertex(e0,v0)
-				mdt.set_vertex(e1,v1)
+#	var mesh = node.get_node("Tile").mesh.duplicate()
+#	var mdt = MeshDataTool.new()
+#	mdt.create_from_surface(mesh, 0)
+#	LineG.clear()
+#	for i in range(mdt.get_edge_count()):
+#		var e0 = mdt.get_edge_vertex(i, 0)
+#		var e1 = mdt.get_edge_vertex(i, 1)
+#		var v0 = mdt.get_vertex(e0)
+#		var v1 = mdt.get_vertex(e1)
+#		var point = {}
+#
+#		for z in range(2):
+#			for j in [[rs1,re1],[rs2,re2],[rs1,rs2],[re1,re2]]:
+#				if G.segment_intersects_segment_2d(R.v3xz(v0),R.v3xz(v1),R.v3xz(j[0]),R.v3xz(j[1])):
+#					var y = G. get_closest_points_between_segments(v0,v1,j[0],j[1])[1].y
+#					v0 = Vector3(v0.x,y,v0.z)
+#					v1 = Vector3(v1.x,y,v1.z)
+#					mdt.set_vertex(e0,v0)
+#					mdt.set_vertex(e1,v1)
+#
+#					LineG.begin(Mesh.PRIMITIVE_LINES)
+#					LineG.add_vertex(v0+node.translation)
+#					LineG.add_vertex(v1+node.translation)
+#					LineG.end()
+#		for z in range(3):
+#			if G.is_point_in_polygon(R.v3xz(v0),rect) or G.is_point_in_polygon(R.v3xz(v1),rect):
+#				var y0 = G.get_closest_point_to_segment(v0,line[0],line[1]).y
+#				var y1 = G.get_closest_point_to_segment(v1,line[0],line[1]).y
+#				v0 = Vector3(v0.x,y0,v0.z)
+#				v1 = Vector3(v1.x,y1,v1.z)
+#				mdt.set_vertex(e0,v0)
+#				mdt.set_vertex(e1,v1)
 #
 #		if G.segment_intersects_segment_2d(R.v3xz(v0),R.v3xz(v1),rect[0],rect[1]):
 #			v0 = Vector3(v0.x,(road_rect[0]-node.translation).y,v0.z)
@@ -212,9 +237,9 @@ func add_road(i):
 #			mdt.set_vertex(e0,v0)
 #			mdt.set_vertex(e1,v1)
 			
-	mesh.surface_remove(0)
-	mdt.commit_to_surface(mesh)
-	node.create_tile_mesh(mesh,false)
+#	mesh.surface_remove(0)
+#	mdt.commit_to_surface(mesh)
+#	node.create_tile_mesh(mesh,false)
 #	node.get_node("Tile").mesh = roadmesh
 	
 #	road_rect[0] = road_rect[road_rect.size()-2]
